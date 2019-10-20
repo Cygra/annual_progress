a517fba6e8282ba4677775b0a4303f08513adeed
Cygra <sjtuwbh@gmail.com>
2019-10-20 15:34:13 +0800

    0.1.0 basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { getProgress } from "./utils";
+import { Basic } from "./components";
 
 const App: React.FC = () => {
+  const [progress, setProgress] = useState<number>(0);
+  useEffect(() => {
+    const interval = setInterval(() => setProgress(getProgress()), 100);
+    return () => clearInterval(interval);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Basic progress={progress} />
     </div>
   );
-}
+};
 
 export default App;

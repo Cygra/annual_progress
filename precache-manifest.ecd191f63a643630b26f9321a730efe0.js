a42726cdc4c7fb7841f737117130b83f72b21774
Cygra <sjtuwbh@gmail.com>
2019-10-20 15:42:10 +0800

    Updates

---
+++ precache-manifest.ecd191f63a643630b26f9321a730efe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d186ec9a150446eb97edb768c2919cf3",
+    "url": "/annual_progress/index.html"
+  },
+  {
+    "revision": "602babc71aa31f5ac460",
+    "url": "/annual_progress/static/css/main.1ab5a605.chunk.css"
+  },
+  {
+    "revision": "6415bcb4f814fbd0e663",
+    "url": "/annual_progress/static/js/2.1be4a487.chunk.js"
+  },
+  {
+    "revision": "602babc71aa31f5ac460",
+    "url": "/annual_progress/static/js/main.d1ea4cd4.chunk.js"
+  },
+  {
+    "revision": "532bc7ce0b7fac68ce1f",
+    "url": "/annual_progress/static/js/runtime-main.95820162.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09df982890795d30cc635bc4d3d7e6ef.js"
+  "/annual_progress/precache-manifest.ecd191f63a643630b26f9321a730efe0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/annual_progress/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
